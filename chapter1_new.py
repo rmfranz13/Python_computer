f5916b810d98f147db26b4225f1eedb4f40b0f9f
Ryan Franz <rmfranz13@gmail.com>
2018-09-19 03:37:07 -0400

    new file:   __pycache__/chapter1_new.cpython-37.pyc 	new file:   __pycache__/fundamentals.cpython-37.pyc 	modified:   chapter1_new.py 	new file:   chapter2_new.py

--- chapter1_new.py
+++ chapter1_new.py
@@ -53,13 +53,13 @@ class Xor:
         return(None)
 
     def update(self):
-        not_a = Not(a)
+        not_a = Not(self.a)
         not_a.update()
-        not_b = Not(b)
+        not_b = Not(self.b)
         not_b.update()
-        and_1 = And(a,not_b.x)
+        and_1 = And(self.a,not_b.x)
         and_1.update()
-        and_2 = And(not_a.x,b)
+        and_2 = And(not_a.x,self.b)
         and_2.update()
         or_gate = Or(and_1.x,and_2.x)
         or_gate.update()
@@ -81,6 +81,25 @@ class And3Way:
         and_2.update()
         self.x = and_2.x
         return(self.x)
+		
+class Or4Way:
+	def __init__(self, a, b, c, d, x=0):
+		self.a = a 
+		self.b = b 
+		self.c = c 
+		self.d = d 
+		self.x = x 
+		return(None)
+		
+	def update(self):
+		or_1 = Or(self.a,self.b)
+		or_1.update()
+		or_2 = Or(self.c,self.d)
+		or_2.update()
+		or_3 = Or(or_1.x,or_2.x)
+		or_3.update()
+		self.x = or_3.x 
+		return(self.x)
 
 class Mux:
     def __init__(self, a, b, sel, x=0):
@@ -158,13 +177,171 @@ class And16:
         and_gates = [i.update() for i in and_gates]
         self.x16 = and_gates
         return(self.x16)
-        
+		
+class Or16:
+	def __init__(self, a16, b16, x16=[0 for i in range(16)]):
+		self.a16 = a16
+		self.b16 = b16
+		self.x16 = x16
+		self.input_list = [[a16[i],b16[i]] for i in range(16)]
+		return(None)
+		
+	def update(self):
+		or_gates = [Or(i[0],i[1]) for i in self.input_list]
+		or_gates = [i.update() for i in or_gates]
+		self.x16 = or_gates
+		return(self.x16)
+		
+class Mux16:
+	def __init__(self, a16, b16, sel16, x16=[0 for i in range(16)]):
+		self.a16 = a16
+		self.b16 = b16
+		self.sel16 = sel16
+		self.x16 = x16
+		self.input_list = [[a16[i],b16[i],sel16[i]] for i in range(16)]
+		return(None)
+		
+	def update(self):
+		muxes = [Mux(i[0],i[1],i[2]) for i in self.input_list]
+		muxes = [i.update() for i in muxes]
+		self.x16 = muxes
+		return(muxes)
+		
+class Or8Way:
+	def __init__(self, a, b, c, d, e, f, g, h, x=0):
+		self.a = a
+		self.b = b
+		self.c = c
+		self.d = d
+		self.e = e
+		self.f = f
+		self.g = g 
+		self.h = h 
+		self.x = x 
+		return(None)
+		
+	def update(self):
+		or1 = Or(self.a,self.b)
+		or1.update()
+		or2 = Or(self.c,self.d)
+		or2.update()
+		or3 = Or(self.e,self.f)
+		or3.update()
+		or4 = Or(self.g,self.h)
+		or4.update()
+		or5 = Or(or1.x,or2.x)
+		or5.update()
+		or6 = Or(or3.x,or4.x)
+		or6.update()
+		or7 = Or(or5.x,or6.x)
+		or7.update()
+		self.x = or7.x
+		return(or7.x)
+		
+class Mux4Way16:
+	def __init__(self, a16, b16, c16, d16, sel2, x16=[0 for i in range(16)]):
+		self.a16 = a16
+		self.b16 = b16
+		self.c16 = c16
+		self.d16 = d16
+		self.sel1 = [sel2[0] for i in range(16)]
+		self.sel2 = [sel2[1]for i in range(16)]
+		self.x16 = x16
+		return(None)
+		
+	def update(self):
+		mux16_1 = Mux16(self.a16,self.b16,self.sel2)
+		mux16_1.update()
+		mux16_2 = Mux16(self.c16,self.d16,self.sel2)
+		mux16_2.update()
+		mux16_3 = Mux16(mux16_1.x16,mux16_2.x16,self.sel1)
+		mux16_3.update()
+		self.x16 = mux16_3.x16
+		return(self.x16)
+		
+class Mux8Way16:
+	def __init__(self, a16, b16, c16, d16, e16, f16, g16, h16, sel3, x16=[0 for i in range(16)]):
+		self.a16 = a16
+		self.b16 = b16
+		self.c16 = c16
+		self.d16 = d16
+		self.e16 = e16
+		self.f16 = f16
+		self.g16 = g16
+		self.h16 = h16
+		self.sel0 = [sel3[0] for i in range(16)]
+		self.sel1 = sel3[1]
+		self.sel2 = sel3[2]
+		self.x16 = x16
+		
+	def update(self):
+	
+		mux416_1 = Mux4Way16(self.a16,self.b16,self.c16,self.d16,[self.sel1,self.sel2])
+		mux416_1.update()
+		mux416_2 = Mux4Way16(self.e16,self.f16,self.g16,self.h16,[self.sel1,self.sel2])
+		mux416_2.update()
+		mux16 = Mux16(mux416_1.x16,mux416_2.x16,self.sel0)
+		mux16.update()
+		self.x16 = mux16.x16
+		return(self.x16)
+		
+class DMux4Way:
+	def __init__(self, a, sel2, w=0, x=0, y=0, z=0):
+		self.a = a
+		self.sel2 = sel2
+		self.w = w
+		self.x = x 
+		self.y = y 
+		self.z = z 
+		return(None)
+		
+	def update(self):
+		dmux_1 = DMux(self.a,self.sel2[0])
+		dmux_1.update()
+		dmux_2 = DMux(dmux_1.x,self.sel2[1])
+		dmux_2.update()
+		dmux_3 = DMux(dmux_1.y,self.sel2[1])
+		dmux_3.update()
+		self.w = dmux_2.x
+		self.x = dmux_2.y 
+		self.y = dmux_3.x 
+		self.z = dmux_3.y 
+		return(self.w,self.x,self.y,self.z)
+		
+class DMux8Way:
+	def __init__(self, a, sel3, x0=0, x1=0, x2=0, x3=0, x4=0, x5=0, x6=0, x7=0):
+		self.a = a 
+		self.sel3 = sel3 
+		self.x0 = x0 
+		self.x1 = x1 
+		self.x2 = x2 
+		self.x3 = x3 
+		self.x4 = x4 
+		self.x5 = x5 
+		self.x6 = x6 
+		self.x7 = x7 
+		return(None)
+		
+	def update(self):
+		dmux_1 = DMux(self.a,self.sel3[0])
+		dmux_1.update()
+		dmux4_1 = DMux4Way(dmux_1.x,[self.sel3[1],self.sel3[2]])
+		dmux4_1.update()
+		dmux4_2 = DMux4Way(dmux_1.y,[self.sel3[1],self.sel3[2]])
+		dmux4_2.update()
+		self.x0 = dmux4_1.w 
+		self.x1 = dmux4_1.x 
+		self.x2 = dmux4_1.y 
+		self.x3 = dmux4_1.z 
+		self.x4 = dmux4_2.w 
+		self.x5 = dmux4_2.x 
+		self.x6 = dmux4_2.y 
+		self.x7 = dmux4_2.z 
+		return(self.x0,self.x1,self.x2,self.x3,self.x4,self.x5,self.x6,self.x7)
+		
+	
 
-a16 = [1,1,0,0,0,0,0,1,0,0,0,0,1,0,0,0]
-b16 = [0,1,0,0,0,0,0,1,0,0,0,0,1,0,0,0]
-and_16 = And16(a16,b16)
-and_16.update()
-print(and_16.x16)
+
 
 
 
---
+++ chapter2_new.py
@@ -0,0 +1,178 @@
+import chapter1_new as ch1
+	
+class HalfAdder:
+	def __init__(self, a, b, carry=0, Sum=0):
+		self.a = a
+		self.b = b 
+		self.carry = carry 
+		self.Sum = Sum 
+		return(None)
+		
+	def update(self):
+		and_1 = ch1.And(self.a,self.b)
+		and_1.update()
+		xor_1 = ch1.Xor(self.a,self.b)
+		xor_1.update()
+		self.carry = and_1.x 
+		self.Sum = xor_1.x 
+		return(self.carry,self.Sum)
+
+class FullAdder:
+	def __init__(self, a, b, c, carry=0, Sum=0):
+		self.a = a 
+		self.b = b 
+		self.c = c 
+		self.carry = carry 
+		self.Sum = Sum 
+		return(None)
+		
+	def update(self):
+		not_a = ch1.Not(self.a)
+		not_a.update()
+		not_b = ch1.Not(self.b)
+		not_b.update()
+		not_c = ch1.Not(self.c)
+		not_c.update()
+		and_1 = ch1.And3Way(not_a.x,not_b.x,self.c)
+		and_1.update()
+		and_2 = ch1.And3Way(not_a.x,self.b,not_c.x)
+		and_2.update()
+		and_3 = ch1.And3Way(not_a.x,self.b,self.c)
+		and_3.update()
+		and_4 = ch1.And3Way(self.a,not_b.x,not_c.x)
+		and_4.update()
+		and_5 = ch1.And3Way(self.a,not_b.x,self.c)
+		and_5.update()
+		and_6 = ch1.And3Way(self.a,self.b,not_c.x)
+		and_6.update()
+		and_7 = ch1.And3Way(self.a,self.b,self.c)
+		and_7.update()
+		or_1 = ch1.Or4Way(and_1.x,and_2.x,and_4.x,and_7.x)
+		or_1.update()
+		or_2 = ch1.Or4Way(and_3.x,and_5.x,and_6.x,and_7.x)
+		or_2.update()
+		self.Sum = or_1.x 
+		self.carry = or_2.x
+		return(self.carry,self.Sum)
+		
+class Add16:
+	def __init__(self, a16, b16, sum16=[0 for i in range(16)]):
+		self.a16 = a16 
+		self.b16 = b16 
+		self.sum16 = sum16 
+		return(None) 
+		
+	def update(self):
+		adder_1 = FullAdder(self.a16[15],self.b16[15],0)
+		adder_1.update()
+		adder_2 = FullAdder(self.a16[14],self.b16[14],adder_1.carry)
+		adder_2.update()
+		adder_3 = FullAdder(self.a16[13],self.b16[13],adder_2.carry)
+		adder_3.update()
+		adder_4 = FullAdder(self.a16[12],self.b16[12],adder_3.carry)
+		adder_4.update()
+		adder_5 = FullAdder(self.a16[11],self.b16[11],adder_4.carry)
+		adder_5.update()
+		adder_6 = FullAdder(self.a16[10],self.b16[10],adder_5.carry)
+		adder_6.update()
+		adder_7 = FullAdder(self.a16[9],self.b16[9],adder_6.carry)
+		adder_7.update()
+		adder_8 = FullAdder(self.a16[8],self.b16[8],adder_7.carry)
+		adder_8.update()
+		adder_9 = FullAdder(self.a16[7],self.b16[7],adder_8.carry)
+		adder_9.update()
+		adder_10 = FullAdder(self.a16[6],self.b16[6],adder_9.carry)
+		adder_10.update()
+		adder_11 = FullAdder(self.a16[5],self.b16[5],adder_10.carry)
+		adder_11.update()
+		adder_12 = FullAdder(self.a16[4],self.b16[4],adder_11.carry)
+		adder_12.update()
+		adder_13 = FullAdder(self.a16[3],self.b16[3],adder_12.carry)
+		adder_13.update()
+		adder_14 = FullAdder(self.a16[2],self.b16[2],adder_13.carry)
+		adder_14.update()
+		adder_15 = FullAdder(self.a16[1],self.b16[1],adder_14.carry)
+		adder_15.update()
+		adder_16 = FullAdder(self.a16[0],self.b16[0],adder_15.carry)
+		adder_16.update()
+		self.sum16[15] = adder_1.Sum 
+		self.sum16[14] = adder_2.Sum 
+		self.sum16[13] = adder_3.Sum 
+		self.sum16[12] = adder_4.Sum 
+		self.sum16[11] = adder_5.Sum 
+		self.sum16[10] = adder_6.Sum 
+		self.sum16[9] = adder_7.Sum 
+		self.sum16[8] = adder_8.Sum 
+		self.sum16[7] = adder_9.Sum 
+		self.sum16[6] = adder_10.Sum 
+		self.sum16[5] = adder_11.Sum 
+		self.sum16[4] = adder_12.Sum 
+		self.sum16[3] = adder_13.Sum 
+		self.sum16[2] = adder_14.Sum 
+		self.sum16[1] = adder_15.Sum 
+		self.sum16[0] = adder_16.Sum 
+		return(self.sum16)
+		
+class Inc16:
+	def __init__(self, a16, sum16=[0 for i in range(16)]):
+		self.a16 = a16
+		self.sum16 = sum16 
+		self.b16 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+		return(None)
+		
+	def update(self):
+		add_16 = Add16(self.a16,self.b16)
+		add_16.update()
+		self.sum16 = add_16.sum16 
+		return(self.sum16)
+		
+		
+class ALU:
+	def __init__(self, a16, b16, zx, nx, zy, ny, f, no, out16 = [0 for i in range(16)]):
+		self.a16 = a16 
+		self.b16 = b16 
+		self.zx = zx 
+		self.nx = nx 
+		self.zy = zy 
+		self.ny = ny 
+		self.f = f 
+		self.no = no 
+		self.zeros = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+		return(None)
+		
+	def update(self): 
+		mux16_1 = ch1.Mux16(self.a16,self.zeros,[self.zx for i in range(16)])
+		mux16_1.update()
+		not16_1 = ch1.Not16(mux16_1.x16)
+		not16_1.update()
+		mux16_2 = ch1.Mux16(mux16_1.x16,not16_1.x16,[self.nx for i in range(16)])
+		mux16_2.update()
+		mux16_3 = ch1.Mux16(self.b16,self.zeros,[self.zy for i in range(16)])
+		mux16_3.update()
+		not16_2 = ch1.Not16(mux16_3.x16)
+		not16_2.update()
+		mux16_4 = ch1.Mux16(mux16_3.x16,not16_2.x16,[self.ny for i in range(16)])
+		mux16_4.update()
+		adder_1 = Add16(mux16_2.x16,mux16_4.x16)
+		adder_1.update()
+		and_1 = ch1.And16(mux16_2.x16,mux16_4.x16)
+		and_1.update()
+		out_0 = ch1.Mux16(and_1.x16,adder_1.sum16,[self.f for i in range(16)])
+		out_0.update()
+		out_1 = ch1.Not16(out_0.x16)
+		out_1.update()
+		out = ch1.Mux16(out_0.x16,out_1.x16,[self.no for i in range(16)])
+		out.update()
+		self.out16 = out.x16
+		return(self.out16)
+		
+
+		
+a16 = [0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1]
+b16 = [0,0,0,1,0,1,0,1,0,1,0,1,0,1,1,1]
+zx,nx,zy,ny,f,no = 0,1,1,1,1,1
+
+ALUy = ALU(a16,b16,zx,nx,zy,ny,f,no)
+ALUy.update()
+print(ALUy.out16)
+		
