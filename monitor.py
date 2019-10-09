a43b362863889ef060f61861122fd6ce07c79b10
Ryan Franz <rmfranz13@gmail.com>
2019-10-09 15:55:07 -0500

    Updates while waiting for vm to reboot

--- monitor.py
+++ monitor.py
@@ -1,34 +1,38 @@
 import tkinter 
-from tkinter import ttk
+#from tkinter import ttk
+import ram as ch3
 
-#lastx, lasty = 0, 0
+class MonitorMM():
+    def __init__():
+        #RAM 512 represents the 512 screen columns.
+        #Each of the 512 RAM512 registers contains 16 bits
+        #Therefore, the 256 screen rows are represented by
+        # 16x16 = 256 bits.
+        self.registers = [ch3.RAM512() for i in range(16)]
 
-def xy(event):
-    global lastx, lasty
-    lastx, lasty = event.x, event.y
+class Monitor():
+    def __init__(self):
+        self.window = tkinter.Tk()
 
-def addLine(event):
-    global lastx, lasty
-    canvas.create_line((lastx, lasty, event.x, event.y))
-    lastx, lasty = event.x, event.y
+        self.canvas = tkinter.Canvas(self.window, width=1024, height=512, bg="#000000")
+        self.canvas.pack()
+        #self.canvas.place(x=0, y=0, height=512, width=1024)
 
-root = tkinter.Tk()
-root.columnconfigure(10, weight=1)
-root.rowconfigure(10, weight=1)
+        ii=0
+        for a in range(1024):
+            for b in range(512):
 
-canvas = tkinter.Canvas(root)
-canvas.place(x=0, y=0, height=256, width=512)
-ii=0
-for a in range(256):
-    for b in range(512):
-        if(ii%5==0):
-            canvas.create_line(a, b, a+1, b+1, fill='black')
-        else:
-            canvas.create_line(a, b, a+1, b+1, fill='white')
-        ii+=1
-#canvas.grid(column=10, row=10)
-canvas.grid(column=0, row=0, sticky=(tkinter.N, tkinter.W, tkinter.E, tkinter.S))
-#canvas.bind("<Button-1>", xy)
-#canvas.bind("<B1-Motion>", addLine)
+                if(ii%2==0):
+                    self.canvas.create_line(a, b, a+1, b+1, fill='black')
+                else:
+                    self.canvas.create_line(a, b, a+1, b+1, fill='white')
+                ii+=1
 
-root.mainloop()
+        #canvas.grid(column=0, row=0, sticky=(tkinter.N, tkinter.W, tkinter.E, tkinter.S))
+
+    def run(self):
+        self.window.mainloop()
+
+#Instantiate and run
+MonitoryTheMonitor = Monitor()
+MonitoryTheMonitor.run()
---
+++ monitor2.py
@@ -0,0 +1,41 @@
+#from tkinter import Tk, Canvas, PhotoImage, mainloop
+import tkinter
+import math
+
+class MonitorMM():
+    def __init__(self):
+        #RAM 512 represents the 512 screen columns.
+        #Each of the 512 RAM512 registers contains 16 bits
+        #Therefore, the 256 screen rows are represented by
+        # 16x16 = 256 bits.
+        self.registers = [ch3.RAM512() for i in range(16)]
+        return(None)
+
+
+class Monitor():
+    def __init__(self):
+        self.MemoryMap = MonitorMM()
+        self.WIDTH = 512
+        self.HEIGHT = 256
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(window, width=self.WIDTH, height=self.HEIGHT, bg = "#000000")
+        self.canvas.pack()
+        self.img = tkinter.PhotoImage(width=self.WIDTH, height=self.HEIGHT)
+        self.canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
+
+#WIDTH, HEIGHT = 640, 480
+    def update(self):
+        for i in self.MemoryMap.re
+
+
+#window = tkinter.Tk()
+#canvas = tkinter.Canvas(window, width=WIDTH, height=HEIGHT, bg="#000000")
+#canvas.pack()
+#img = tkinter.PhotoImage(width=WIDTH, height=HEIGHT)
+#canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
+
+for x in range(4 * WIDTH):
+    y = int(HEIGHT/2 + HEIGHT/4 * math.sin(x/80.0))
+    img.put("#ffffff", (x//4,y))
+
+tkinter.mainloop()
