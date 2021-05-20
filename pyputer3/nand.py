f6792c030adf29eb9b2e423153f34dab0eccfec3
rfranz <rfranz@intuitivemachines.com>
2021-05-20 20:37:55 +0000

    Here we go again

---
+++ pyputer3/nand.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+# Basic Nand gate underlying everything else
+
+class Nand:
+    def __init__(self):
+        self.pin_a = 0
+        self.pin_b = 0
+        self.pin_x = 0
+
+    def update(self):
+        if(self.pin_a and self.pin_b):
+            self.pin_x = 1
+        else:
+            self.pin_x = 0
